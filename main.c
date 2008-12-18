2d874f6ab048366a63410696bc58b37652e30183
koike <pneumaster@gmail.com>
2008-12-18 14:17:56 +0900

    test C source

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void sub(double arg, ...)
+{
+	va_list ap;
+	int turn;
+
+	va_start(ap, arg);
+
+	turn = va_arg(ap, int);
+	printf("%d\n", turn);
+
+	va_end(ap);
+}
+
+
+int main()
+{
+	double f;
+
+	// sub(f, 0);
+	sub(f);
+	return 0;
+}
